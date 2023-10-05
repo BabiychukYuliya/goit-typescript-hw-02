d36f062e06f50b7b7869f7c84b09f03997559fe3
BabiychukYuliya <julia4233097@gmail.com>
2023-10-05 16:39:58 +0300

    3,4

---
+++ src/baseTypes/4.js
@@ -0,0 +1,8 @@
+"use strict";
+/*
+  Як переписати його в TypeScript, використовуючи концепцію кортежів,
+  щоб гарантувати, що перший елемент завжди буде рядком, а другий числом?
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+var person;
+person = ["Max", 21];
